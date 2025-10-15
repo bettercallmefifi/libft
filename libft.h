5d0a0a614624b28ffb952258a49aecd220b68451
Ferdaous El Idrissi <feel-idr@e1r13p1.1337.ma>
2025-10-15 09:12:11 +0100

    libft

---
+++ libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feel-idr <feel-idr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 21:52:21 by feel-idr          #+#    #+#             */
+/*   Updated: 2025/10/14 22:43:13 by feel-idr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef  LIBFT_H
+# define LIBFT_h
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdee.h>
+#include <string.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
